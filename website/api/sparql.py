d6d4840e2794a8daddc37ab6bca3bc8e316e1995
Samuel Stroschein <samuelstroschein@outlook.com>
2020-10-14 00:38:26 +0200

    test

--- website/api/sparql.py
+++ website/api/sparql.py
@@ -9,9 +9,37 @@ app = Flask(__name__)
 @app.route('/<path:path>')
 def catch_all(path):
     req_query = request.args.get('query')
+    print(req_query)
     g = Graph()
     g.parse("vu_studyguide.ttl", format="turtle")
-    qres = g.query(req_query)
-    serialized = qres.serialize(format='json-ld', indent=4)
-    print(serialized)
-    return Response(qres.serialize(format='json-ld', indent=4))
+    qres = g.query("""
+               PREFIX vu: <https://www.vu.nl/en/properties/>
+PREFIX vuc: <https://studiegids.vu.nl/en/2020-2021/courses/>
+PREFIX teach: <http://linkedscience.org/teach/ns#>
+PREFIX dbo: <http://dbpedia.org/ontology/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+select DISTINCT * where { 
+	?course teach:ects ?credits ;
+         	vu:courseLevel ?level;
+          	teach:academicTerm ?period;
+           	teach:courseTitle ?title;
+            vu:taughtBy ?teacher;
+            dbo:language ?language;
+            teach:grading ?grading;
+            vu:courseContent ?content;
+            rdf:type teach:Course;
+            vu:courseObjective ?objective;
+            vu:offeredByFaculty ?faculty;
+            vu:literature ?literature;
+            vu:teachingMethods ?teachingMethod.
+} LIMIT 100
+               """)
+    result = None
+    for row in qres:
+        result = row
+        break
+    return Response(result)
+    # qres = g.query(req_query)
+    # serialized = qres.serialize(format='json-ld', indent=4)
+    # print(serialized)
+    # return Response(qres.serialize(format='json-ld', indent=4))
---
+++ website/api/test.py
@@ -0,0 +1,29 @@
+from rdflib import Graph, RDF, Namespace, Literal, URIRef
+
+g = Graph()
+g.parse("vu_studyguide.ttl", format="turtle")
+qres = g.query("""
+               PREFIX vu: <https://www.vu.nl/en/properties/>
+PREFIX vuc: <https://studiegids.vu.nl/en/2020-2021/courses/>
+PREFIX teach: <http://linkedscience.org/teach/ns#>
+PREFIX dbo: <http://dbpedia.org/ontology/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+select DISTINCT * where { 
+	?course teach:ects ?credits ;
+         	vu:courseLevel ?level;
+          	teach:academicTerm ?period;
+           	teach:courseTitle ?title;
+            vu:taughtBy ?teacher;
+            dbo:language ?language;
+            teach:grading ?grading;
+            vu:courseContent ?content;
+            rdf:type teach:Course;
+            vu:courseObjective ?objective;
+            vu:offeredByFaculty ?faculty;
+            vu:literature ?literature;
+            vu:teachingMethods ?teachingMethod.
+} LIMIT 100
+               """)
+for row in qres:
+    print(row)
+    break
