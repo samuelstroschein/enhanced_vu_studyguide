c83c5b5116fb9f0ef6c48248b6583873c842404b
Samuel Stroschein <samuelstroschein@outlook.com>
2020-10-13 23:38:19 +0200

    included graph in response

--- website/api/sparql.py
+++ website/api/sparql.py
@@ -9,32 +9,11 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    res = request.args.get('query')
-    return Response(res)
+    req_query = request.args.get('query')
+    g = Graph()
 
+    g.parse("vu_studyguide.ttl", format="turtle")
+    # print(g.serialize(format="turtle").decode("utf-8"))
+    qres = g.query(req_query)
+    return Response(qres)
 
-# g = Graph()
-
-# g.parse("vu_studyguide.ttl", format="turtle")
-# # print(g.serialize(format="turtle").decode("utf-8"))
-# qres = g.query(
-#     """PREFIX vu: <https://www.vu.nl/en/properties/>
-# PREFIX vuc: <https://studiegids.vu.nl/en/2020-2021/courses/>
-# PREFIX teach: <http://linkedscience.org/teach/ns#>
-# PREFIX dbo: <http://dbpedia.org/ontology/>
-# PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-# select DISTINCT * where {
-#     ?course teach:ects ?credits ;
-#             vu:courseLevel ?level;
-#             teach:academicTerm ?period;
-#             teach:courseTitle ?title;
-#             vu:taughtBy ?teacher;
-#             dbo:language ?language;
-#             teach:grading ?grading;
-#             vu:courseContent ?content;
-#             rdf:type teach:Course;
-#             vu:courseObjective ?objective;
-#             vu:offeredByFaculty ?faculty;
-#             vu:literature ?literature;
-#             vu:teachingMethods ?teachingMethod.
-# } LIMIT 1""")
