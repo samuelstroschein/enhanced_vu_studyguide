73b86eabea77dc385c2268b50896d24cf1859cc7
Samuel Stroschein <samuelstroschein@outlook.com>
2020-10-14 00:51:41 +0200

    qres readded

--- website/api/sparql.py
+++ website/api/sparql.py
@@ -15,28 +15,7 @@ with open('vu_studyguide_pickled.pk', 'rb') as fi:
 def catch_all(path):
     req_query = request.args.get('query')
     print(req_query)
-    qres = g.query("""
-               PREFIX vu: <https://www.vu.nl/en/properties/>
-PREFIX vuc: <https://studiegids.vu.nl/en/2020-2021/courses/>
-PREFIX teach: <http://linkedscience.org/teach/ns#>
-PREFIX dbo: <http://dbpedia.org/ontology/>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-select DISTINCT * where { 
-	?course teach:ects ?credits ;
-         	vu:courseLevel ?level;
-          	teach:academicTerm ?period;
-           	teach:courseTitle ?title;
-            vu:taughtBy ?teacher;
-            dbo:language ?language;
-            teach:grading ?grading;
-            vu:courseContent ?content;
-            rdf:type teach:Course;
-            vu:courseObjective ?objective;
-            vu:offeredByFaculty ?faculty;
-            vu:literature ?literature;
-            vu:teachingMethods ?teachingMethod.
-} LIMIT 100
-               """)
+    qres = g.query(qres)
     result = None
     for row in qres:
         result = row
