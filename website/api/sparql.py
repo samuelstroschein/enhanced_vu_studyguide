c4fc6bf1d6006c7ec5e4ee6340932f25f98f419c
Samuel Stroschein <samuelstroschein@outlook.com>
2020-10-13 23:41:42 +0200

    .

--- website/api/sparql.py
+++ website/api/sparql.py
@@ -2,7 +2,6 @@ from http.server import BaseHTTPRequestHandler
 from rdflib import Graph, RDF, Namespace, Literal, URIRef
 from flask import Flask, Response, request
 
-
 app = Flask(__name__)
 
 
@@ -11,9 +10,6 @@ app = Flask(__name__)
 def catch_all(path):
     req_query = request.args.get('query')
     g = Graph()
-
     g.parse("vu_studyguide.ttl", format="turtle")
-    # print(g.serialize(format="turtle").decode("utf-8"))
     qres = g.query(req_query)
-    return Response(qres)
-
+    return Response(qres[0])
