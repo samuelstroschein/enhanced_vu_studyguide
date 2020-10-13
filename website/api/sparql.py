4ee65fd23cd56856e5c153d2b600d21b66f419e7
Samuel Stroschein <samuelstroschein@outlook.com>
2020-10-13 23:27:49 +0200

    created sparql endpoint

--- website/api/sparql.py
+++ website/api/sparql.py
@@ -1,31 +1,39 @@
+from http.server import BaseHTTPRequestHandler
 from rdflib import Graph, RDF, Namespace, Literal, URIRef
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-g = Graph()
 
-g.parse("vu_studyguide.ttl", format="turtle")
-# print(g.serialize(format="turtle").decode("utf-8"))
-qres = g.query(
-    """PREFIX vu: <https://www.vu.nl/en/properties/>
-PREFIX vuc: <https://studiegids.vu.nl/en/2020-2021/courses/>
-PREFIX teach: <http://linkedscience.org/teach/ns#>
-PREFIX dbo: <http://dbpedia.org/ontology/>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-select DISTINCT * where { 
-	?course teach:ects ?credits ;
-         	vu:courseLevel ?level;
-          	teach:academicTerm ?period;
-           	teach:courseTitle ?title;
-            vu:taughtBy ?teacher;
-            dbo:language ?language;
-            teach:grading ?grading;
-            vu:courseContent ?content;
-            rdf:type teach:Course;
-            vu:courseObjective ?objective;
-            vu:offeredByFaculty ?faculty;
-            vu:literature ?literature;
-            vu:teachingMethods ?teachingMethod.
-} LIMIT 1""")
+class handler(BaseHTTPRequestHandler):
 
-for row in qres:
-    print(row)
+    def do_GET(self):
+        g = Graph()
+
+        g.parse("vu_studyguide.ttl", format="turtle")
+        # print(g.serialize(format="turtle").decode("utf-8"))
+        qres = g.query(
+            """PREFIX vu: <https://www.vu.nl/en/properties/>
+        PREFIX vuc: <https://studiegids.vu.nl/en/2020-2021/courses/>
+        PREFIX teach: <http://linkedscience.org/teach/ns#>
+        PREFIX dbo: <http://dbpedia.org/ontology/>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        select DISTINCT * where { 
+            ?course teach:ects ?credits ;
+                    vu:courseLevel ?level;
+                    teach:academicTerm ?period;
+                    teach:courseTitle ?title;
+                    vu:taughtBy ?teacher;
+                    dbo:language ?language;
+                    teach:grading ?grading;
+                    vu:courseContent ?content;
+                    rdf:type teach:Course;
+                    vu:courseObjective ?objective;
+                    vu:offeredByFaculty ?faculty;
+                    vu:literature ?literature;
+                    vu:teachingMethods ?teachingMethod.
+        } LIMIT 1""")
+        message = qres[0]
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(message.encode())
+        return
