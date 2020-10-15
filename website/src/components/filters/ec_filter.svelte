ee0e83e5e2c72e8485e1fc781837f1e477e09beb
BC-Tim <tim12juli2001@gmail.com>
2020-10-15 22:04:12 +0200

    Cap improvements

--- website/src/components/filters/ec_filter.svelte
+++ website/src/components/filters/ec_filter.svelte
@@ -7,9 +7,9 @@
 
 <div class="flex justify-center">
   <label class="inline-flex items-center mt-3">
-    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$ecFilter} value={"?credits = '3'^^xsd:integer"}>
+    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$ecFilter} value={"?Credits = '3'^^xsd:integer"}>
     <span class="ml-2">{"3EC"}</span>
-    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$ecFilter} value={"?credits = '6'^^xsd:integer"}>
+    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$ecFilter} value={"?Credits = '6'^^xsd:integer"}>
     <span class="ml-2">{"6EC"}</span>
     <br>
   </label>
--- website/src/components/filters/level_filter.svelte
+++ website/src/components/filters/level_filter.svelte
@@ -7,17 +7,17 @@
 
 <div class="flex justify-center">
   <label class="grid gap-4 grid-cols-4">
-    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$levelFilter} value={"?level = '100'^^xsd:integer"}>
+    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$levelFilter} value={"?Level = '100'^^xsd:integer"}>
     <span class="ml-2">{"100"}</span>
-    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$levelFilter} value={"?level = '200'^^xsd:integer"}>
+    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$levelFilter} value={"?Level = '200'^^xsd:integer"}>
     <span class="ml-2">{"200"}</span>
-    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$levelFilter} value={"?level = '300'^^xsd:integer"}>
+    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$levelFilter} value={"?Level = '300'^^xsd:integer"}>
     <span class="ml-2">{"300"}</span>
-    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$levelFilter} value={"?level = '400'^^xsd:integer"}>
+    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$levelFilter} value={"?Level = '400'^^xsd:integer"}>
     <span class="ml-2">{"400"}</span>
-    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$levelFilter} value={"?level = '500'^^xsd:integer"}>
+    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$levelFilter} value={"?Level = '500'^^xsd:integer"}>
     <span class="ml-2">{"500"}</span>
-    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$levelFilter} value={"?level = '600'^^xsd:integer"}>
+    <input class="form-radio h-5 w-8 text-blue-600" type=radio bind:group={$levelFilter} value={"?Level = '600'^^xsd:integer"}>
     <span class="ml-2">{"600"}</span>
     <br>
   </label>
