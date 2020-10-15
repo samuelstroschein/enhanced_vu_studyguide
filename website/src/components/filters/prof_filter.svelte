27b26e6350774639d0d2a759ef904702485577e6
BC-Tim <tim12juli2001@gmail.com>
2020-10-15 22:14:29 +0200

    Updated ttl file

--- website/src/components/filters/prof_filter.svelte
+++ website/src/components/filters/prof_filter.svelte
@@ -1,5 +1,56 @@
 <script>
   import Select from "smelte/src/components/Select";
+
+
+  
+  async function FetchProf(){
+    
+    var mySparqlEndpoint = "https://enhanced-vu-studyguide.vercel.app/api/sparql" ;
+    var mySparqlQuery = `select DISTINCT * { 
+    ?Course rdf:type teach:Course;
+            teach:academicTerm ?Period;
+              vu:courseLevel ?Level;
+            teach:ects ?Credits;
+            vu:taughtBy ?Teacher;
+              dbo:language ?Language;
+              teach:courseTitle ?Title.
+            OPTIONAL {
+            ?Course teach:grading ?Grading;
+              vu:courseContent ?Content;
+              vu:courseObjective ?Objective;
+              vu:offeredByFaculty ?FacultyName;
+              vu:literature ?Literature;
+              vu:teachingMethods ?Teaching_Method.
+            ?FacultyName rdfs:label ?Faculty.
+      }   	
+    FILTER (${$ecFilter})
+    FILTER (${$levelFilter})
+    FILTER (${$periodFilter})
+  }LIMIT 100`;
+    var response = await fetch(mySparqlEndpoint + "?query=" + mySparqlQuery, {
+    method: "GET",
+    headers: {'Accept':'application/sparql-results+json', 'Content-Type':'application/sparql-results+json'}
+    })
+    var json = await response.json()
+    $queryResponse = [];
+    checkResponse(json.results.bindings)
+  }
+
+	function checkResponse(bindings){
+		var result = {}
+		bindings.forEach(course => {
+			var parsedJson = {
+				text: course.Title.value,
+				items: []
+			}
+			for (var property in course){
+				if (property == "Title") continue
+				parsedJson.items.push({ text: property + ": " + course[property].value})
+			}
+			// console.log(parsedJson)
+			$queryResponse = [...$queryResponse, parsedJson]
+		});
+	}
   
   let value1 = "";
   let value2 = "";
