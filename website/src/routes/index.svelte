340b520e4a374e3908f33ec91acca83072175584
Samuel Stroschein <samuelstroschein@outlook.com>
2020-10-15 23:31:52 +0200

    improved layout

--- website/src/routes/index.svelte
+++ website/src/routes/index.svelte
@@ -1,15 +1,21 @@
+<Header class="mb-20" />
+<div class="grid gap-4 grid-cols-10">
+  <div class="col-span-2">
+    <div>
+      <Filters />
+    </div>
+    <div>
+      <GoButton />
+    </div>
+  </div>
+  <div class="col-span-8">
+    <Results />
+  </div>
+</div>
+
 <script>
   import Results from "../components/results.svelte";
   import Header from "../components/header.svelte";
   import Filters from "../components/filters.svelte";
   import GoButton from "../components/go_button.svelte";
 </script>
-
-<div><Header /></div>
-<div class="grid gap-4 grid-cols-10">
-  <div class="col-span-2">
-    <div><Filters /></div>
-    <div><GoButton /></div>
-  </div>
-  <div class="col-span-8"><Results /></div>
-</div>
