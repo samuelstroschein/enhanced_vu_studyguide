21955a618883dfd43524afc0d7e341e433538d08
Samuel Stroschein <samuelstroschein@outlook.com>
2020-10-17 18:46:50 +0200

    formatting

--- website/src/components/go_button.svelte
+++ website/src/components/go_button.svelte
@@ -1,6 +1,10 @@
 <div class="flex justify-center py-8">
-  <div class="px-2"><Button color="blue" on:click={SparQL}>Start Search</Button></div>
-  <div class="px-2"><Button color="blue" on:click={reset}>Reset</Button></div>
+  <div class="px-2">
+    <Button color="blue" on:click={SparQL}>Start Search</Button>
+  </div>
+  <div class="px-2">
+    <Button color="blue" on:click={reset}>Reset</Button>
+  </div>
 </div>
 
 <!-- DEBUG STATEMENTS BELOW -->
@@ -21,7 +25,6 @@
 <div>
 <p>DEBUG Language: {$languageFilter}</p>
 </div> -->
-
 <script>
   import Button from "smelte/src/components/Button";
   import { queryResponse } from "../store.js";
@@ -58,7 +61,8 @@
 	FILTER (${$levelFilter})
 	FILTER (${$periodFilter})
 	FILTER (${$languageFilter})
-}LIMIT 100`;
+}
+LIMIT 1000`;
     } else {
       var mySparqlQuery = `select DISTINCT ?StudieGids_URL ?Period ?Level ?Credits ?Teacher ?Language ?Title ?Grading ?Content ?Objective ?Literature ?Teaching_Method ?Faculty { 
 		    ?StudieGids_URL rdf:type teach:Course;
@@ -85,47 +89,44 @@
 	FILTER (${$languageFilter})
 }`;
     }
-console.log(mySparqlQuery)
     var response = await fetch(mySparqlEndpoint + "?query=" + mySparqlQuery, {
       method: "GET",
       headers: {
         Accept: "application/sparql-results+json",
-        "Content-Type": "application/sparql-results+json"
-      }
+        "Content-Type": "application/sparql-results+json",
+      },
     });
     var json = await response.json();
     $queryResponse = [];
-    console.log(json.results.bindings.length);
-    if(json.results.bindings.length != 0){
+    if (json.results.bindings.length != 0) {
       checkResponse(json.results.bindings);
+    } else {
+      $queryResponse = [
+        { text: "There were no results that matched your filter" },
+      ];
     }
-    else{
-      $queryResponse = [{text: "There were no results that matched your filter"}];
-    }
-    
-    
   }
 
   function checkResponse(bindings) {
     var result = {};
-    bindings.forEach(course => {
+    bindings.forEach((course) => {
       var parsedJson = {
         text: course.Title.value,
-        items: []
+        items: [],
       };
       for (var property in course) {
         if (property == "Title") continue;
         parsedJson.items.push({
-          text: property + ": " + course[property].value
+          text: property + ": " + course[property].value,
         });
       }
       // console.log(parsedJson)
       $queryResponse = [...$queryResponse, parsedJson];
     });
-    console.log($queryResponse)
+    console.log($queryResponse);
   }
 
-  function reset(){
+  function reset() {
     location.reload();
   }
 </script>
